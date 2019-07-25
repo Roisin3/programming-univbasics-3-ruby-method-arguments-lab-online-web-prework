98f09f6a3d71c6c77cbeb2865b949a3eb9620c75
Michael Lynch <michael@michaelrlynch.com>
2019-07-25 20:04:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,13 +13,9 @@ introduction_with_language("Dan", "Ember.js")
 introduction_with_language("Josh", "React")
 
 
-def introduction_with_language_optional (name, language)
-  if language == ""
-    puts "Hi, my name is #{name} and I am learning to program in Ruby.\n"
-  else
-    puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
-  end
+def introduction_with_language_optional (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-#introduction_with_language_optional("Edwin","")
-#introduction_with_language_optional("Edwin", "Python")
+introduction_with_language_optional("Edwin","")
+introduction_with_language_optional("Edwin", "Python")
